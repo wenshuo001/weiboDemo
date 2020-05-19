851917652f8fba3b303095752d759fe04f56b0af
wenshuo <1443678932@qq.com>
2020-05-19 23:22:06 +0800

    Initial Commit

---
+++ weiboDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weiboDemo
+//
+//  Created by Wenshuo on 2020/5/19.
+//  Copyright © 2020 Wenshuo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
