5af55637be3712f6ebdadba92111a5f99997e1c0
wenshuo <1443678932@qq.com>
2020-05-24 18:42:55 +0800

    first commit

---
+++ weiboDemo/PostCell.swift
@@ -0,0 +1,21 @@
+//
+//  PostCell.swift
+//  weiboDemo
+//
+//  Created by Wenshuo on 2020/5/20.
+//  Copyright © 2020 Wenshuo. All rights reserved.
+//
+
+import SwiftUI
+
+struct PostCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostCell_Previews: PreviewProvider {
+    static var previews: some View {
+        PostCell()
+    }
+}
---
+++ weiboDemo/PostVIPBadge.swift
@@ -0,0 +1,21 @@
+//
+//  PostVIPBadge.swift
+//  weiboDemo
+//
+//  Created by Wenshuo on 2020/5/24.
+//  Copyright © 2020 Wenshuo. All rights reserved.
+//
+
+import SwiftUI
+
+struct PostVIPBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostVIPBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        PostVIPBadge()
+    }
+}
